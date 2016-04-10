007635c6a4e3e4b103613bc5a5f82820d35f2258
Cloud <pizzafist@gmx.de>
2016-04-10 15:28:07 +0200

    Real Spacemeth has curves

--- 02_Cardnumber/main.c
+++ 02_Cardnumber/main.c
@@ -5,10 +5,10 @@ int main()
 {
     unsigned num_fsc_cards;
     unsigned num_lib_cards;
-    printf("Enter the number of fascist cards: ",num_fsc_cards);
+    printf("Enter the number of fascist cards: ", num_fsc_cards);
     scanf("%d", &num_fsc_cards);
-    printf("Enter the number of liberal cards: ",num_lib_cards);
+    printf("Enter the number of liberal cards: ", num_lib_cards);
     scanf("%d", &num_lib_cards);
-    printf("There are %d cards in total",num_fsc_cards+num_lib_cards);
+    printf("There are %d cards in total", num_fsc_cards+num_lib_cards);
     return 0;
 }
