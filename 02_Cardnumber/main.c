5a3285315578cc74a681ffa4d5e9db0c4beeb999
Cloud <pizzafist@gmx.de>
2016-04-10 14:53:28 +0200

    i can do meth

---
+++ 02_Cardnumber/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("There are 11 fascist cards\n");
+    printf("There are 6 liberal cards\n");
+    printf("There are %d cards in total",11+6);
+    return 0;
+}
